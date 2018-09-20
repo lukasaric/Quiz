58dc922ec3be71ab15b84e6a09972c0829da94a5
Luka <rpg.ari7@gmail.com>
2018-09-20 14:51:50 +0200

    Test new design look

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -1,33 +1,54 @@
 <template>
-  <div class="hero-body">
-    <div class="columns">
-      <div id="result" class="column has-text-white " style="display: none">
+  <v-stepper non-linear>
+    <v-stepper-header>
+      <div
+        v-for="(step, index) in steps"
+        :key="index">
+        <v-stepper-step
+          :step="steps[index]"
+          editable
+          color="orange accent-3">
+          Question {{ steps[index] }}
+        </v-stepper-step>
       </div>
-    </div>
-    <div id="columnId" class="columns" >
-      <div class="column has-text-white">
-        <div v-for="(question, index) in quiz" :key="index">
-          <div v-show="index === questionIndex">
-            <h2 id="subtitle">{{ question.text }}</h2>
-            <!-- Responses: display a li for each possible response with a checkbox button -->
-            <ul>
-              <li v-for="(answer, j) in question.answers" :key="j">
-                <label>
-                  <input :value="answer.id" v-model="checkedAnswers" @click="sendIndex(answer)" type="checkbox"> {{ answer.text }}
-                </label>
-              </li>
-            </ul>
-            <a v-if="questionIndex !== 4" @click="next" class="button is-pulled-right">
-              Next
-            </a>
-            <a v-if="questionIndex === 4" @click="submit" class="button is-pulled-right">
-              Submit
-            </a>
-          </div>
+    </v-stepper-header>
+
+    <v-stepper-items
+      v-for="(question, index) in quiz"
+      :key="index">
+      <v-stepper-content :step="steps[index]">
+        <v-card
+          class="mb-5"
+          color="blue-grey lighten-5"
+          height="200px">
+          <h2 id="subtitle">{{ question.text }}</h2>
+        </v-card>
+        <div
+          v-for="(answer, j) in question.answers"
+          :key="j">
+          <v-checkbox
+            :label="answer.text"
+            :value="answer.id"
+            v-model="checkedAnswers"
+            color="orange darken-3">
+          </v-checkbox>
         </div>
-      </div>
-    </div>
-  </div>
+        <v-btn
+          v-if="steps[index] !== 5"
+          @click="next"
+          color="orange accent-3">
+          Continue
+        </v-btn>
+        <v-btn flat>Cancel</v-btn>
+        <v-btn
+          v-if="steps[index] === 5"
+          @click="submit"
+          color="green accent-3">
+          Submit
+        </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
 </template>
 
 <script>
@@ -37,6 +58,7 @@ export default {
   name: 'test',
   data() {
     return {
+      steps: [1, 2, 3, 4, 5],
       quiz,
       questionIndex: 0,
       questionStage: false,
@@ -48,18 +70,16 @@ export default {
   },
   methods: {
     next() {
-      // console.log(this.arrayToSend);
+      this.steps[this.questionIndex] = this.index;
       this.questionIndex++;
       this.index = 0;
     },
     submit() {
-      // console.log(this.arrayToSend);
       let response1 = 'Results: <br>';
       let response2 = 0;
       document.getElementById('columnId').style.display = 'none';
       document.getElementById('result').style.display = 'block';
       const uri = '/topic/submit';
-      // this.axios.post(uri, this.arrayToSend).then((response)
       Api.post(uri, this.arrayToSend).then(response => {
         response.data.forEach(element => {
           response1 += String(element.result) + '<br>';
@@ -76,8 +96,8 @@ export default {
   created() {
     Api.get(`topic/${this.$store.state.topicId}`)
       .then(res => {
+        console.log(res.data);
         this.quiz = res.data;
-        // console.log(this.quiz);
         this.quiz.forEach((element, i) => {
           this.arrayToSend.push({ questId: element.id, answers: [] });
         });
@@ -86,39 +106,4 @@ export default {
 };
 </script>
 <style scoped>
-  .hero-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  }
-
-  .column {
-    background-color: #003333;
-    padding: 1rem 2rem 1rem;
-  }
-
-  .column h2 {
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-  }
-
-  .column li {
-  font-size: 1.2rem;
-  margin-bottom: 0.5rem;
-  }
-
-  .button {
-    margin-top: 1rem;
-    margin-right: 0.5rem;
-  }
-
-  #result {
-    border-radius: 15px;
-    background-color:#003333;
-    max-width: 480px;
-    margin: auto;
-    margin-top: 9rem;
-    text-align: center;
-    font-size: 2.25rem;
-  }
 </style>
